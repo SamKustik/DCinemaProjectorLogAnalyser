09318239d3dd801c5942811e6efa4d594bed757d
SamKustik <49914169+SamKustik@users.noreply.github.com>
2019-05-04 10:35:06 +0300

    Updated main app logic
    Added projector parameters recognition. Minor optimization. Added some comments. YEY - changed version number.

--- 1st_pre_alfa_version.py
+++ 1st_pre_alfa_version.py
@@ -1,5 +1,5 @@
 # Log analyser for some digital cinema projectors
-# ver.0.00.00.08
+# ver.0.00.00.10
 # written by Samvel
 
 from typing import Set
@@ -14,7 +14,7 @@ errnum_list = []  # dictionary with all the errors
 # in this loop we will find all the errors
 for line in open('ProjectorLog.txt', 'r'):
     for c in line:
-        if logfilecontent[charcount] == 'err':
+        if logfilecontent[charcount:charcount+3] == 'err':
             errorcount += 1
         # this statement will find all the error numbers
         if logfilecontent[charcount] == '#' and logfilecontent[charcount+1] == '5':
@@ -36,12 +36,48 @@ choosed = input('print \'y\' or \'n\': ')
 if choosed == 'y':
     for line in open('ProjectorLog.txt', 'r'):
         for c in line:
-            if logfilecontent[charcount:charcount+3] == 'e':
+            if logfilecontent[charcount:charcount+3] == 'err':
                 print(line)
             charcount += 1
 else:
     print('OK, so we are closing the file.')
 logfile.close()
+
+
+# new function that will find some keywords in parameters file to reveal all the projector info
+# I will add some clarity for variables a little bit later
+# but believe me - it is working for now
+# yes - there is some bugs
+# but this is pre-pre-pre-alfa version and at present stage it is working normally
+# I'm working on optimization and - SURPRISE - GUI version on Qt
+def wordfinder(z, x, q, v, b, n):
+    ch = 0
+    m = ''
+    for u in z:
+        if z[ch:ch + q] == x:
+            for t in z[ch + v:ch + b]:
+                if t != n:
+                    m = m + t
+                elif t == n:
+                    break
+        ch += 1
+    return(m)
+
+
+# next few lines of code are defining several parameters of the projector
+# keywords and numeric values for these keywords are ALWAYS the same
+# because parameters file has always the same structure
+parameters = open('Parameters.txt', 'r')  # opening of standard Parameters file
+parameterscontent = parameters.read()
+
+print("Your projector runtime is " + wordfinder(parameterscontent, 'Projector runtime', 17, 19, 24, '\"'))
+
+print("Your projector model is " + wordfinder(parameterscontent, 'Identifier', 10, 12, 30, '\"'))
+
+print("Your projector lamp type is " + wordfinder(parameterscontent, 'Lamp runtime', 12, 14, 20, '\"'))
+
+print("Your projector lens type is " + wordfinder(parameterscontent, 'Lens description', 16, 18, 78, '\"'))
+
+parameters.close()
+
 print('The logfile was successfully opened and closed.')
-
-
